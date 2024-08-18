d04170ddd6a9952016dfd9030457c9053d693dcd
warlock2k <achyuth.amar@gmail.com>
2024-08-18 13:25:12 -0400

    refactoring for better readability

--- bootloader.asm
+++ bootloader.asm
@@ -4,19 +4,18 @@ ORG 0x7C00        ; Origin, BIOS loads the bootloader here
 
 start:
     mov si, msg   ; Load address of message into SI register
+    mov ah, 0x0E  ; BIOS teletype function to print a character
     call print    ; Call the print function
 
 hang:
     jmp hang      ; Infinite loop to halt execution
 
 print:
-    mov ah, 0x0E  ; BIOS teletype function to print a character
-.print_loop:
     lodsb         ; Load byte at address SI into AL, increment SI
     cmp al, 0
     je .done      ; If the byte is zero (end of string), jump to done
     int 0x10      ; BIOS interrupt to print character in AL
-    jmp .print_loop
+    jmp print
 
 .done:
     ret           ; Return from print function
